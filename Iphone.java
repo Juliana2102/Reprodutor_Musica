899b71feb41e2ef6e1d1f75b0a30405d22e2de4a
Juliana Ribeiro <julianari.mendes@gmail.com>
2025-07-01 07:15:39 -0300

    Atualizar o Iphone.java

--- Iphone.java
+++ Iphone.java
@@ -5,18 +5,18 @@ public class Iphone {
     private int numeroSerie;
 
     public void ligar() {
-        // Lógica para ligar o iPhone
+        
     }
 
     public void desligar() {
-        // Lógica para desligar o iPhone
+       
     }
 
     public void tocarMusica() {
-        // Lógica para tocar música
+        
     }
 
     public void ajustarVolume() {
-        // Lógica para ajustar o volume
+        
     }
 }

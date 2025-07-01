f34f5fe234a77788fbda65c7ef2f427115fae2df
Juliana Ribeiro <julianari.mendes@gmail.com>
2025-07-01 07:14:48 -0300

    Atualizar o Musica.java

--- Musica.java
+++ Musica.java
@@ -5,15 +5,15 @@ public class Musica {
     private String arquivoAudio;
 
     public void reproduzir() {
-        // Lógica para reproduzir a música
+       
     }
 
     public void pausar() {
-        // Lógica para pausar a música
+       
     }
 
     public void parar() {
-        // Lógica para parar a música
+        
     }
 
 }
